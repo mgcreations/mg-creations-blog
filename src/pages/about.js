0159439f791b15258bbf89becfdb57482d8ea498
mgcreations <info@mgcreations.co.za>
2021-09-24 09:25:42 +0200

    Part 2 Done

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About This Site">
+      <p>Follow along and let's create something amazing!</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
